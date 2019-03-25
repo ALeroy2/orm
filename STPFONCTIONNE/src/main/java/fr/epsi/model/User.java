af550dc53abe06ac784fc8eea327d13d9fde30d3
Alexis LEROY <39801664+ALeroy2@users.noreply.github.com>
2019-03-25 15:48:11 +0100

    Add files via upload

---
+++ STPFONCTIONNE/src/main/java/fr/epsi/model/User.java
@@ -0,0 +1,61 @@
+package fr.epsi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String firstname;
+    private String lastname;
+    private Date datenais;
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Date getDatenais() {
+        return datenais;
+    }
+
+    public void setDatenais(Date datenais) {
+        this.datenais = datenais;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
